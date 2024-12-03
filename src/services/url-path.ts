d63674517ed71466fff101a5e48247076bfe0c23
dung.nguyen <dung.nguyen3@bmbsoft.com.vn>
2024-12-03 15:14:37 +0700

    INIT

--- src/services/url-path.ts
+++ src/services/url-path.ts
@@ -1,6 +1,7 @@
 // const BASE_URL = import.meta.env.VITE_BASE_URL;
 const URL_PATHS = {
   DELETE_EXTERNAL_API_BY_ID: "api/ExternalAPI/DeleteExternalAPI",
+  GET_CURRENT_USER: ''
 };
 
 export default URL_PATHS;
